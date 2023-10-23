52419da3727f0d36e3a039003baca4e0cd2cd1ae
tamani phiri <tamanigabriel0@gmail.com>
2023-10-24 01:51:41 +0200

    c

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ i_hash = input("Enter password : ")
 p_doc=input("\nEnter password filename including path  : \n")
 
 p_file=open(p_doc,'r')
+
+for word in p_file:
+    enc_word=word.encode('utf-8')
+    hash_word=hashlib.md5(enc_word.strip())
+    digest=hash_word.hexdigest()
+    
+    
+    if digest==i_hash:
+        print("")
